8715ee69c436e0a674eb3b805ed5a1037e4acade
Claudio <claudio.raimondi@protonmail.com>
2024-06-08 13:09:24 +0200

    Add start and end

--- srcs/headers/a_star.hpp
+++ srcs/headers/a_star.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   constants.hpp                                      :+:      :+:    :+:   */
+/*   a_star..hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 18:49:01 by craimond          #+#    #+#             */
-/*   Updated: 2024/06/07 18:49:38 by craimond         ###   ########.fr       */
+/*   Updated: 2024/06/08 13:02:24 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,24 @@
 
 #include <cstdint>
 #include <array>
+#include <SFML/Graphics.hpp>
+#include <cstring>
 
-constexpr uint8_t TILE_SIZE = 20; // Size of a tile in pixels
-constexpr uint8_t COLS = 40; // Number of columns
-constexpr uint8_t ROWS = 30; // Number of rows
+constexpr uint8_t TILE_SIZE = 30; // Size of a tile in pixels
+constexpr uint8_t COLS = 30; // Number of columns
+constexpr uint8_t ROWS = 20; // Number of rows
+
+enum e_cell : uint8_t
+{
+	FREE = 0,
+	OBSTACLE = 1,
+	START = 2,
+	END = 3,
+};
+
+static const std::unordered_map<e_cell, sf::Color> cell_colors = {
+	{FREE, sf::Color::White},
+	{OBSTACLE, sf::Color::Black},
+	{START, sf::Color::Green},
+	{END, sf::Color::Red},
+};
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,37 +6,49 @@
 /*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 17:08:21 by craimond          #+#    #+#             */
-/*   Updated: 2024/06/08 11:59:42 by craimond         ###   ########.fr       */
+/*   Updated: 2024/06/08 13:08:07 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <SFML/Graphics.hpp>
-#include "headers/constants.hpp"
-#include <iostream>
-#include <cstring>
+#include "headers/a_star.hpp"
 
 using std::array, std::pair;
 
 static void	init_window(sf::RenderWindow &window);
-static void	display_grid(sf::RenderWindow &window, bool grid[COLS][ROWS]);
-static void	fill_map(sf::RenderWindow &window, bool grid[COLS][ROWS]);
+static void	display_grid(sf::RenderWindow &window, enum e_cell grid[COLS][ROWS]);
+static void	set_obstacles(sf::RenderWindow &window, enum e_cell grid[COLS][ROWS]);
+static void	set_pointed_cell(enum e_cell grid[COLS][ROWS], const enum e_cell status, sf::RenderWindow &window);
+static void	update_start(enum e_cell grid[COLS][ROWS], sf::RenderWindow &window);
+static void	update_end(enum e_cell grid[COLS][ROWS], sf::RenderWindow &window);
+static void	visualize_pathfinding(sf::RenderWindow &window, enum e_cell grid[COLS][ROWS]);
 
 int main(void)
 {
 	sf::RenderWindow	window;
-	bool				grid[COLS][ROWS] = {0};
+	enum e_cell	grid[COLS][ROWS] = {FREE}; //0 = free, 1 = obstacle, 2 = start, 3 = end, gradients = costs
 	sf::Event			event;
 	bool				simulation_started = false;
 
 	init_window(window);
+	display_grid(window, grid);
 	while (window.isOpen())
 	{
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				goto cleanup;
-			else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
-				simulation_started = true;
+			if (!simulation_started)
+			{
+				if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::S)
+					update_start(grid, window);
+				else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::E)
+					update_end(grid, window);
+				else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
+				{
+					simulation_started = true;
+					visualize_pathfinding(window, grid);
+				}
+			}
 			else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::R)
 			{
 				simulation_started = false;
@@ -44,10 +56,8 @@ int main(void)
 				display_grid(window, grid);
 			}
 		}
-		if (simulation_started)
-			visualize_pathfinding(window, grid); //blocca il programma fino alla fine della simulazione (dopo di puo premere x o r)
-		else
-			fill_map(window, grid);
+		if (!simulation_started)
+			set_obstacles(window, grid);
 	}
 cleanup:
 	window.clear();
@@ -63,17 +73,15 @@ static void init_window(sf::RenderWindow &window)
 	window.setFramerateLimit(15);
 }
 
-static void	display_grid(sf::RenderWindow &window, bool grid[COLS][ROWS])
+static void	display_grid(sf::RenderWindow &window, enum e_cell grid[COLS][ROWS])
 {
 	sf::RectangleShape	rect(sf::Vector2f(TILE_SIZE, TILE_SIZE));
-	sf::Color			color;
 
 	for (uint16_t i = 0; i < COLS; i++)
 	{
 		for (uint16_t j = 0; j < ROWS; j++)
 		{
-			color = (grid[i][j] == 0) ? sf::Color::Red : sf::Color::Black;
-			rect.setFillColor(color);
+			rect.setFillColor(cell_colors.at(grid[i][j]));
 			rect.setPosition(i * TILE_SIZE, j * TILE_SIZE); //TODO ottimizzabilissimo (constexpr)
 			window.draw(rect);
 		}
@@ -81,22 +89,59 @@ static void	display_grid(sf::RenderWindow &window, bool grid[COLS][ROWS])
 	window.display();
 }
 
-static void	fill_map(sf::RenderWindow &window, bool grid[COLS][ROWS])
+static void	set_obstacles(sf::RenderWindow &window, enum e_cell grid[COLS][ROWS])
 {
-	const bool				left_click = sf::Mouse::isButtonPressed(sf::Mouse::Left);
-	const bool				right_click = sf::Mouse::isButtonPressed(sf::Mouse::Right);
-	static sf::Vector2i		mouse_pos;
-	static sf::Vector2i		tile; //TODO ottimizzabile con pair
+	const bool	left_click = sf::Mouse::isButtonPressed(sf::Mouse::Left);
+	const bool	right_click = sf::Mouse::isButtonPressed(sf::Mouse::Right);
 
 	if (!left_click && !right_click)
 		return ;
-	mouse_pos = sf::Mouse::getPosition(window);
-	tile.x = mouse_pos.x / TILE_SIZE;
-	tile.y = mouse_pos.y / TILE_SIZE;
-	if (tile.x >= 0 && tile.x < COLS && tile.y >= 0 && tile.y < ROWS)
-	{
-		grid[tile.x][tile.y] = left_click ? 1 : 0;
-		display_grid(window, grid);
-	}
+
+	const enum e_cell	status = (left_click) ? OBSTACLE : FREE;
+
+	set_pointed_cell(grid, status, window);
+}
+
+static void update_start(enum e_cell grid[COLS][ROWS], sf::RenderWindow &window)
+{
+	static sf::Vector2i	start = {-1, -1};
+	const sf::Vector2i	mouse_pos = sf::Mouse::getPosition(window);
+	const sf::Vector2i	tile = {mouse_pos.x / TILE_SIZE, mouse_pos.y / TILE_SIZE};
+
+	grid[start.x][start.y] = FREE;
+	grid[tile.x][tile.y] = START;
+	start.x = tile.x;
+	start.y = tile.y;
+	display_grid(window, grid);
+}
+
+static void update_end(enum e_cell grid[COLS][ROWS], sf::RenderWindow &window)
+{
+	static sf::Vector2i	end = {-1, -1};
+	const sf::Vector2i	mouse_pos = sf::Mouse::getPosition(window);
+	const sf::Vector2i	tile = {mouse_pos.x / TILE_SIZE, mouse_pos.y / TILE_SIZE};
+
+	grid[end.x][end.y] = FREE;
+	grid[tile.x][tile.y] = END;
+	end.x = tile.x;
+	end.y = tile.y;
+	display_grid(window, grid);
+}
+
+static void	set_pointed_cell(enum e_cell grid[COLS][ROWS], const enum e_cell status, sf::RenderWindow &window)
+{
+	const sf::Vector2i	mouse_pos = sf::Mouse::getPosition(window);
+	const sf::Vector2i	tile = {mouse_pos.x / TILE_SIZE, mouse_pos.y / TILE_SIZE};
+
+	grid[tile.x][tile.y] = status;
+	display_grid(window, grid);
+}
+
+static void visualize_pathfinding(sf::RenderWindow &window, enum e_cell grid[COLS][ROWS])
+{
+
+	(void)window;
+	(void)grid;
+	//TODO
 }
 
